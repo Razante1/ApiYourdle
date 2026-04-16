0c5d1b89a4b3d9d9f091ad912e3c9cc6ff602fff
Antonio Guilherme Macedo Camara <antonioguilhermemc@gmail.com>
2026-04-15 22:17:44 -0300

    updatE: correção de sevice

--- src/services/llmService.js
+++ src/services/llmService.js
@@ -1,25 +1,10 @@
 // src/services/llmService.js
 export class LLMService {
     async generateGameData(theme) {
-      const response = await fetch(process.env.N8N_WEBHOOK_URL, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ theme })
-      })
-  
-      if (!response.ok) {
-        throw new Error("Erro ao chamar n8n")
+      // Simulação de resposta da IA
+      return {
+        items: ["Jason", "Tommy", "Kimberly", "Billy"],
+        secret: "Tommy"
       }
-  
-      const data = await response.json()
-  
-      // validação básica
-      if (!data.items || !data.secret) {
-        throw new Error("Resposta inválida do n8n")
-      }
-  
-      return data
     }
   }
